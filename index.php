a25acd5a8f1d85343f61983e579b51bfb0e14022
flavio022 <flavio.danilo022@gmail.com>
2017-12-29 00:11:25 -0200

    adding logo image

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="/css/main.css">
+  <link rel="stylesheet" href="css/main.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
@@ -20,15 +20,45 @@
               <li><a href="#">Shirts</a></li>
               <li><a href="#">Pants</a></li>
               <li><a href="#">Shoes</a></li>
-              <li><a href="#">Accessories</a></li>
-
-              
+              <li><a href="#">Accessories</a></li>              
             </ul>
           </li>
       </div>
+    </nav>    
+    <div id="headerWrapper">
+    <div id="back-flower"></div>
+    <div id="logotext"></div>
+    <div id="fore-flower"></div>
+    </div>
+    <p>O que é Lorem Ipsum?
+Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.
 
-    </nav>  
+Porque nós o usamos?
+É um fato conhecido de todos que um leitor se distrairá com o conteúdo de texto legível de uma página quando estiver examinando sua diagramação. A vantagem de usar Lorem Ipsum é que ele tem uma distribuição normal de letras, ao contrário de "Conteúdo aqui, conteúdo aqui", fazendo com que ele tenha uma aparência similar a de um texto legível. Muitos softwares de publicação e editores de páginas na internet agora usam Lorem Ipsum como texto-modelo padrão, e uma rápida busca por 'lorem ipsum' mostra vários websites ainda em sua fase de construção. Várias versões novas surgiram ao longo dos anos, eventualmente por acidente, e às vezes de propósito (injetando humor, e coisas do gênero).
 
+
+De onde ele vem?
+Ao contrário do que se acredita, Lorem Ipsum não é simplesmente um texto randômico. Com mais de 2000 anos, suas raízes podem ser encontradas em uma obra de literatura latina clássica datada de 45 AC. Richard McClintock, um professor de latim do Hampden-Sydney College na Virginia, pesquisou uma das mais obscuras palavras em latim, consectetur, oriunda de uma passagem de Lorem Ipsum, e, procurando por entre citações da palavra na literatura clássica, descobriu a sua indubitável origem. Lorem Ipsum vem das seções 1.10.32 e 1.10.33 do "de Finibus Bonorum et Malorum" (Os Extremos do Bem e do Mal), de Cícero, escrito em 45 AC. Este livro é um tratado de teoria da ética muito popular na época da Renascença. A primeira linha de Lorem Ipsum, "Lorem Ipsum dolor sit amet..." vem de uma linha na seção 1.10.32.
+
+O trecho padrão original de Lorem Ipsum, usado desde o século XVI, está reproduzido abaixo para os interessados. Seções 1.10.32 e 1.10.33 de "de Finibus Bonorum et Malorum" de Cicero também foram reproduzidas abaixo em sua forma exata original, acompanhada das versões para o inglês da tradução feita por H. Rackham em 1914.</p> 
+<script>
+  jQuery(window).scroll(function(){
+    var vscroll = jQuery(this).scrollTop();
+    jQuery('#logotext').css({
+      "transform" : "translate(0px, "+vscroll/2+"px)"
+    });
+    var vscroll = jQuery(this).scrollTop();
+    jQuery('#back-flower').css({
+      "transform" : "translate("+vscroll/5+"px, -"+vscroll/12+"px)"
+    });
+
+    var vscroll = jQuery(this).scrollTop();
+    jQuery('#fore-flower').css({
+      "transform" : "translate(0px, -"+vscroll/2+"px)"
+    });
+
+  })
+</script>
 
 </body>
 </html>
